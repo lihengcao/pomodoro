12f014008d6034a92d5d362dec8dc6a94fdd7f9b
Liheng <lihengcao@gmail.com>
2022-07-18 18:10:50 -0500

    rudimentary timer behavior

---
+++ README.md
@@ -0,0 +1,7 @@
+# pomodoro
+Rudimentary CLI pomodoro timer
+
+## Goals
+- Make a simple pomodoro timer app
+- Practice Rust
+- Maybe make a GUI in the future
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::{io, time, io::Write};
+
+fn main() {
+    // probaby better to have this as a global in the future
+    // probably be good to have this as an array so the match statement (or equivalent) can be done programmatically 
+    // let options = "[a: 1] [b: 5] [c: 10] [d: 15] [exit]";
+
+    println!("Welcome to pomodoro!"); // One time startup messsage
+
+    loop { // main application loop
+        let input = get_input(
+            "Choose from the following timer lengths or enter a custom time:\n[a: 1] [b: 5] [c: 10] [d: 15] [exit]"
+        );
+        
+        match input.as_str() {
+            "exit" => break,
+            "a" => start_timer(1),
+            "b" => start_timer(5),
+            "c" => start_timer(10),
+            "d" => start_timer(15),
+            _ => println!("Try again!") // haven't implemented custom times yet
+        }
+    }
+}
+
+fn get_input(prompt: &str) -> String {
+    let mut line = String::new(); // initialize output
+    println!("{prompt}");
+
+    io::stdin().read_line(&mut line).expect("bad input");
+
+    return line.trim().to_string();
+
+}
+
+fn start_timer(duration: u64) {
+    let start = time::Instant::now();
+
+    for t in (0..duration + 1).rev() {
+        while time::Instant::now() < start + time::Duration::new(duration - t, 0) {} // do nothing/wait for one second
+        // !TODO measure length of output
+        // for not, just assuming that no number will be more than 5 digits
+        print!("\r{}", " ".repeat(5)); // clear previous output
+        print!("\r{t}"); // print current number
+        io::stdout().flush().expect("error flushing");
+    }
+    println!(); // print newline
+}
+
+// fn printflush(str: String) {
+
+// }
+
